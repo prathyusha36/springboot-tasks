10959c641eac0a747cf61116805c3096d55390d8
prathyusha36 <39513592+prathyusha36@users.noreply.github.com>
2019-08-07 15:22:47 +0530

    updated state

--- src/main/java/com/stackroute/muzixApp/service/TrackSerciceImpl.java
+++ src/main/java/com/stackroute/muzixApp/service/TrackSerciceImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Service
 public class TrackSerciceImpl implements TrackService{
-    TrackRepository trackRepository;
+    private TrackRepository trackRepository;
     @Autowired
     public TrackSerciceImpl(TrackRepository trackRepository) {
         this.trackRepository=trackRepository;
