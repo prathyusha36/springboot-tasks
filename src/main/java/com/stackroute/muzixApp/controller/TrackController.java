5b49501777d5137ef1bf584458ccb9ef2ec410ce
prathyusha36 <39513592+prathyusha36@users.noreply.github.com>
2019-08-07 12:57:20 +0530

    updated method name

--- src/main/java/com/stackroute/muzixApp/controller/TrackController.java
+++ src/main/java/com/stackroute/muzixApp/controller/TrackController.java
@@ -15,59 +15,48 @@ import java.util.List;
 @RestController
 @RequestMapping(value="api/v1")
 public class TrackController {
-    @Autowired
     private TrackService trackService;
-
+    private TrackSerciceImpl trackSercice;
     public TrackController(TrackService trackService) {
 
         this.trackService=trackService;
     }
 
     @PostMapping("track")
-    public ResponseEntity<?> saveUser(@RequestBody Track track) throws UserAlreadyExistsException{
+    public ResponseEntity<?> saveTrack(@RequestBody Track track) throws UserAlreadyExistsException{
         ResponseEntity responseEntity;
         trackService.saveTrack(track);
         responseEntity = new ResponseEntity<String>("successfully created", HttpStatus.CREATED);
         return responseEntity;
     }
 
-    @PostMapping("tracks")
-    public ResponseEntity<?> saveUser1(@RequestBody List<Track> track) throws UserAlreadyExistsException{
-        ResponseEntity responseEntity;
-       for(Track t:track) {
-           trackService.saveTrack(t);
-       }
-        responseEntity = new ResponseEntity<List<Track>>(trackService.getAllTracks(), HttpStatus.CREATED);
-        return responseEntity;
-    }
-
     @GetMapping("track")
-    public ResponseEntity<?> getAllUsers() {
+    public ResponseEntity<?> getAllTracks() {
         return new ResponseEntity<List<Track>>(trackService.getAllTracks(), HttpStatus.OK);
     }
 
     @GetMapping("track/{id}")
-    public ResponseEntity<?> getUserById(@PathVariable("id") int id)throws UserNotFoundException {
+    public ResponseEntity<?> getTrackById(@PathVariable("id") int id)throws UserNotFoundException {
         ResponseEntity responseEntity;
         responseEntity = new ResponseEntity<Track>(trackService.getTrackById(id), HttpStatus.CREATED);
         return responseEntity;
         }
 
     @DeleteMapping("track/{id}")
-    public ResponseEntity<?> deleteUser(@PathVariable("id") int id) {
+    public ResponseEntity<?> deleteTrack(@PathVariable("id") int id) {
         trackService.deleteTrack(id);
         return new ResponseEntity<String>("deleted", HttpStatus.FORBIDDEN);
     }
 
     @PutMapping("track")
-    public ResponseEntity<?> updateUser(@RequestBody Track track) {
+    public ResponseEntity<?> updateTrack(@RequestBody Track track) {
         trackService.updateTrack(track);
         return new ResponseEntity<Track>(track, HttpStatus.OK);
     }
 
-    @GetMapping("track/name/{name}")
-    public ResponseEntity<?> getTrackByName(@PathVariable(value="name") String name) {
-        List<Track> getTrack=trackService.findByName(name);
-        return new ResponseEntity<List<Track>>(getTrack,HttpStatus.OK);
+    @RequestMapping(method=RequestMethod.GET)
+    public ResponseEntity<?> getTrackByName(@RequestParam(value="name") String name) {
+        Track getTrack=(Track)trackService.findByName(name);
+        return new ResponseEntity<Track>(getTrack,HttpStatus.OK);
     }
 }
