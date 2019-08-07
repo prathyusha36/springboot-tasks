67b14060b6a0794bca9a4e71580288398b1531c0
prathyusha36 <39513592+prathyusha36@users.noreply.github.com>
2019-08-07 12:28:18 +0530

    updated method name

--- src/main/java/com/stackroute/muzixApp/service/TrackSerciceImpl.java
+++ src/main/java/com/stackroute/muzixApp/service/TrackSerciceImpl.java
@@ -45,7 +45,7 @@ public class TrackSerciceImpl implements TrackService{
     }
 
     @Override
-    public Track updateUser(Track track) {
+    public Track updateTrack(Track track) {
        return trackRepository.save(track);
     }
 }
